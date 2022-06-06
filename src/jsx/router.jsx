77c6a66d72aa77b737d6b6554636d6430cf868ea
Shaiz Kunhimohammed <k.shaiz@gmail.com>
2022-06-06 19:11:50 -0400

    repo rename

--- src/jsx/router.jsx
+++ src/jsx/router.jsx
@@ -1,35 +1,35 @@
-const $PUBLIC_URL= '';
+const $PUBLIC_URL= 'cliniopsmui';
 
 const RouterLink = ({
   product: {
-    _0: `${$PUBLIC_URL}/products/`,
-    _1: `${$PUBLIC_URL}/products/#cliniops-connect`,
-    _2: `${$PUBLIC_URL}/products/#cliniops-edge`,
-    _3: `${$PUBLIC_URL}/products/#cliniops-conduct`,
+    _0: `/${$PUBLIC_URL}/products/`,
+    _1: `/${$PUBLIC_URL}/products/#cliniops-connect`,
+    _2: `/${$PUBLIC_URL}/products/#cliniops-edge`,
+    _3: `/${$PUBLIC_URL}/products/#cliniops-conduct`,
   },
   service: {
-    _0: `${$PUBLIC_URL}/services/`,
-    _1: `${$PUBLIC_URL}/services/study-setup/`,
-    _2: `${$PUBLIC_URL}/services/clinical-data-management/`,
-    _3: `${$PUBLIC_URL}/services/biostatistics-programming/`,
+    _0: `/${$PUBLIC_URL}/services/`,
+    _1: `/${$PUBLIC_URL}/services/study-setup/`,
+    _2: `/${$PUBLIC_URL}/services/clinical-data-management/`,
+    _3: `/${$PUBLIC_URL}/services/biostatistics-programming/`,
   },
   about: {
     // _0: ${$PUBLIC_URL}`/about/`,
-    _1: `${$PUBLIC_URL}/about/mission/`,
-    _2: `${$PUBLIC_URL}/about/leadership-team/`,
-    _3: `${$PUBLIC_URL}/about/careers/`,
-    _3_1: `${$PUBLIC_URL}/about/careers/project-manager/`
+    _1: `/${$PUBLIC_URL}/about/mission/`,
+    _2: `/${$PUBLIC_URL}/about/leadership-team/`,
+    _3: `/${$PUBLIC_URL}/about/careers/`,
+    _3_1: `/${$PUBLIC_URL}/about/careers/project-manager/`
   },
   media: {
-    _0: `${$PUBLIC_URL}/media/`,
-    _1: `${$PUBLIC_URL}/media/newsroom/`,
-    _2: `${$PUBLIC_URL}/media/blogs/`,
-    _2_1: `${$PUBLIC_URL}/media/blog/diversity-in-clinical-research`
+    _0: `/${$PUBLIC_URL}/media/`,
+    _1: `/${$PUBLIC_URL}/media/newsroom/`,
+    _2: `/${$PUBLIC_URL}/media/blogs/`,
+    _2_1: `/${$PUBLIC_URL}/media/blog/diversity-in-clinical-research`
   },
   contact: {
-    _0: `${$PUBLIC_URL}/contact/`,
-    _1: `${$PUBLIC_URL}/contact/request-quote`,
-    _2: `${$PUBLIC_URL}/contact/request-demo/`
+    _0: `/${$PUBLIC_URL}/contact/`,
+    _1: `/${$PUBLIC_URL}/contact/request-quote`,
+    _2: `/${$PUBLIC_URL}/contact/request-demo/`
     // mailto:info@cliniops.com
   }
 });
