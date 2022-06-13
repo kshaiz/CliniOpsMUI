0a95d4ca6251f5706e02e8be9f19c8a6126ee097
Shaiz Kunhimohammed <k.shaiz@gmail.com>
2022-06-12 20:08:42 -0400

    checkpoint

--- src/jsx/template/contact/_0/_2.jsx
+++ src/jsx/template/contact/_0/_2.jsx
@@ -9,7 +9,7 @@ const template = () => {
   return (
     <>
       <Stack spacing={2}>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d404860.6630182387!2d-121.983968!3d37.555678!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fc085d455ae0b%3A0xf3c0cc7a328847d5!2sMurco%20Building%2C%2038750%20Paseo%20Padre%20Pkwy%2C%20Fremont%2C%20CA%2094536!5e0!3m2!1sen!2sus!4v1628171696500!5m2!1sen!2sus" width="100%" height="120px" style={{border:0, borderRadius: '8px'}} allowFullScreen="" loading="lazy"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5319.380887986923!2d-121.98377985553265!3d37.556882602828736!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fc085d455ae0b%3A0xf3c0cc7a328847d5!2sMurco%20Building%2C%2038750%20Paseo%20Padre%20Pkwy%2C%20Fremont%2C%20CA%2094536!5e0!3m2!1sen!2sus!4v1655078304751!5m2!1sen!2sus" width="100%" height="120px" style={{border:0, borderRadius: '8px'}} allowFullScreen="" loading="lazy"></iframe>
         <Box>
           <Typography variant="h4">USA Contact</Typography>
           <Stack spacing={1} sx={{ mt: 1 }}>
